50854d45efa7537c98b0555b2ba44bc3b010de5f
Morten Bertz <morten@telethon.jp>
2016-08-31 12:09:12 +0900

    Initial Commit

---
+++ APNG/APNG.h
@@ -0,0 +1,19 @@
+//
+//  APNG.h
+//  APNG
+//
+//  Created by Morten Bertz on 8/31/16.
+//  Copyright © 2016 telethon k.k. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for APNG.
+FOUNDATION_EXPORT double APNGVersionNumber;
+
+//! Project version string for APNG.
+FOUNDATION_EXPORT const unsigned char APNGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APNG/PublicHeader.h>
+
+
